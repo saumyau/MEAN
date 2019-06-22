dccdf05d42e1ed395d5381d9a96e5e6b979ce4e4
Saumya <sharmasaumya026@gmail.com>
2019-06-22 19:46:27 -0400

    Added express and node

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Post } from '../post.model';
 import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -13,7 +14,6 @@ export class PostCreateComponent {
 
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated = new EventEmitter<Post>();
 
   // onAddPost(postInput: HTMLTextAreaElement) {
   // // alert('Post added!');
@@ -22,12 +22,12 @@ export class PostCreateComponent {
   // onAddPost1() {
   //   this.newPost = this.enteredValue;
   // }
-
+  constructor( public postsService: PostsService ) {}
   onAddPost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    const post: Post = { title: form.value.title, content: form.value.content };
-    this.postCreated.emit(post);
+    this.postsService.addPost( form.value.title, form.value.content );
+    form.resetForm();
   }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,17 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector : 'app-post-list',
   templateUrl : './post-list.component.html',
   styleUrls : ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit {
   // posts = [
   //   {title: 'First Post', content: 'This is first\'s post'},
   //   {title: 'Second Post', content: 'This is second post'},
   //   {title: 'Third Post', content: 'This is third post'},
   //   {title: 'Fourth Post', content: 'This is fourth post'}
   // ];
-  @Input() posts = [];
+  posts: Post[] = [];
+  private postsSub: Subscription;
+  constructor( public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
 }
